e2caf7728e796e65406125a25f3396e5f3d78912
Mateus GONCALVES <mgoncalv@e1r11p13.42.fr>
2022-02-21 21:45:25 +0100

    minimal code necessary to read a map and write on terminal

--- main.c
+++ main.c
@@ -6,16 +6,28 @@
 /*   By: mgoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 20:01:40 by mgoncalv          #+#    #+#             */
-/*   Updated: 2022/02/21 21:12:02 by mgoncalv         ###   ########.fr       */
+/*   Updated: 2022/02/21 21:44:23 by mgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minilibx/mlx.h"
 #include <stdio.h>
+#include <unistd.h>
+#include "gnl/get_next_line.h"
+
 int	main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
-	printf("Hello World!\n");
+	int		fd;
+	char	*line;
+	if (argc != 2)
+		return (1);
+	fd = open(argv[1], O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
 	return (0);
 }
