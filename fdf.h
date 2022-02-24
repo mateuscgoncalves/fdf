54967ae30f24738c60ae8d4163c8529cdb75e5f6
Mateus GONCALVES <mgoncalv@e1r9p18.42.fr>
2022-02-24 16:56:01 +0100

    create t_point and refactor bres alg

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: mgoncalv <mgoncalv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 19:26:49 by mgoncalv          #+#    #+#             */
-/*   Updated: 2022/02/23 19:45:40 by mgoncalv         ###   ########.fr       */
+/*   Updated: 2022/02/24 16:22:21 by mgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,19 @@
 #include <unistd.h>
 #include "libft/libft.h"
 #include "parsemap/parsemap.h"
+
+
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+typedef struct s_point {
+	int	x;
+	int	y;
+}	t_point;
 
 #endif
--- main.c
+++ main.c
@@ -6,12 +6,11 @@
 /*   By: mgoncalv <mgoncalv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 20:01:40 by mgoncalv          #+#    #+#             */
-/*   Updated: 2022/02/24 16:10:42 by mgoncalv         ###   ########.fr       */
+/*   Updated: 2022/02/24 16:26:28 by mgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -21,37 +20,42 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *) dst = color;
 }
 
-void	ft_bres_alg(int x0, int y0, int x1, int y1, t_data *data)
+void	ft_bres_alg(t_point point0, t_point point1, t_data *data)
 {
-	int	dx = abs(x1 - x0);
-	int	sx = -1;
-	if (x0 < x1)
-		sx = 1;
-	int dy = -abs(y1 - y0);
-	int sy = -1;
-	if (y0 < y1)
-		sy = 1;
-	int error = dx + dy;
+	t_point	d;
+	t_point	s;
+	int error;
 	int e2;
-
-	while (1)
+	int	brk;
+	
+	d.x = abs(point1.x - point0.x);
+	s.x = -1;
+	if (point0.x < point1.x)
+		s.x = 1;
+	d.y = -abs(point1.y - point0.y);
+	s.y = -1;
+	if (point0.y < point1.y)
+		s.y = 1;
+	error = d.x + d.y;
+	brk = 0;
+	while (!brk)
 	{
-		my_mlx_pixel_put(data, x0, y0, 0x00FF0000);
-		if (x0 == x1 && y0 == 1)
-			break;
+		my_mlx_pixel_put(data, point0.x, point0.y, 0x00FF0000);
+		if (point0.x == point1.x && point0.y == 1)
+			brk = 1;
 		e2 = 2 * error;
-		if (e2 >= dy)
+		if (e2 >= d.y)
 		{
-			if (x0 == x1)
-				break;
-			error = error + dy;
-			x0 = x0 + sx;
-		} else if (e2 <= dx)
+			if (point0.x == point1.x)
+				brk = 1;
+			error = error + d.y;
+			point0.x = point0.x + s.x;
+		} else if (e2 <= d.x)
 		{
-			if (y0 == y1)
-				break;
-			error = error + dx;
-			y0 = y0 + sy;
+			if (point0.y == point1.y)
+				brk = 1;
+			error = error + d.x;
+			point0.y = point0.y + s.y;
 		}
 	}
 }
@@ -60,6 +64,8 @@ int    main(void)
     void    *mlx;
     void    *mlx_win;
     t_data    img;
+	t_point point;
+	t_point point2;
 
     mlx = mlx_init();
     mlx_win = mlx_new_window(mlx, 1000, 1000, "FDF");
@@ -67,10 +73,16 @@ int    main(void)
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
                                  &img.endian);
 
-    ft_bres_alg(100, 500, 500, 500, &img);
-	ft_bres_alg(100, 100, 500, 100, &img);
-	ft_bres_alg(100, 100, 100, 500, &img);
-	ft_bres_alg(500, 100, 500, 500, &img);
+	point.x = 100;
+	point.y = 500;
+
+	point2.x = 200;
+	point2.y = 600;
+
+    ft_bres_alg(point, point2, &img);
+	// ft_bres_alg(100, 100, 500, 100, &img);
+	// ft_bres_alg(100, 100, 100, 500, &img);
+	// ft_bres_alg(500, 100, 500, 500, &img);
     mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
     mlx_loop(mlx);
 }
